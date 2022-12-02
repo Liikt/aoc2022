69c18eec663de4a72733e27b15e05b5aeb9726d3
Julian Beier <jb@futureplay.de>
2022-12-02 07:47:16 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::env::args;
+use std::process::exit;
+use std::fs::read_to_string;
+
+macro_rules! day {
+    ($day:ident, $day_str:expr) => {
+        println!("{} part 1: {}", $day_str, $day::solve1(
+            read_to_string("inputs/".to_owned() + $day_str).unwrap()));
+        println!("{} part 2: {}", $day_str, $day::solve2(
+            read_to_string("inputs/".to_owned() + $day_str).unwrap()));
+    };
+}
+
+fn main() {
+    if args().len() != 2 {
+        println!("Usage: ./{} <all|dayX>", args().next().unwrap());
+        exit(1);
+    }
+
+    match args().nth(1).unwrap().as_str() {
+        "all" => {
+        },
+        _ => panic!("Invalid day as argument")
+    }
+}
